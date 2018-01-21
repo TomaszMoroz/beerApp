8ee80e65502df8fd3675c9baef336bdbe762cc3e
TomaszMoroz <tmoroz688@gmail.com>
2018-01-21 16:03:48 +0100

    app structure- first atempt

---
+++ src/components/Beers.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Beers extends Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    Available Beers
+                </h1>
+            </div>
+        )
+    }
+
+}
+
+export default Beers;
